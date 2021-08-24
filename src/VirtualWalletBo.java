dd97b66294d257c21c2a130f7afce5998a72075c
Xuewen.Lei <Xuewen.Lei@ff.com>
2021-08-24 18:06:44 +0800

    update 12

---
+++ src/VirtualWalletBo.java
@@ -0,0 +1,9 @@
+/**
+ * @author xuewen.lei
+ * @title: VirtualWalletBo
+ * @projectName Design_Pattern
+ * @description: TODO
+ * @date 2021/8/24 18:06
+ */
+public class VirtualWalletBo {
+}
---
+++ src/VirtualWalletController.java
@@ -0,0 +1,9 @@
+/**
+ * @author xuewen.lei
+ * @title: VirtualWalletController
+ * @projectName Design_Pattern
+ * @description: TODO
+ * @date 2021/8/24 18:05
+ */
+public class VirtualWalletController {
+}
